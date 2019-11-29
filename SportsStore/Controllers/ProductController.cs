b927cd6c87f3395aa05221a40eeb04253f4d92e0
divelnick <shabanovamir11@gmail.com>
2019-11-29 13:32:26 +0300

    first commit

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public int PageSize = 4;
+
+        public ProductController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ViewResult List(int productPage = 1)
+            => View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((productPage - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            });
+    }
+}
---
+++ SportsStore/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authentication;
+
+namespace SportsStore.Models.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
